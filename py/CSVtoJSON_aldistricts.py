e0e7d485af4d1e134ad6a98a646c371fbf18500e
Emily Merwin <emilymerwinajc@gmail.com>
2013-11-11 19:32:29 -0500

    Create ID fields in JSON to match up with SVG IDs

--- py/CSVtoJSON_aldistricts.py
+++ py/CSVtoJSON_aldistricts.py
@@ -5,8 +5,19 @@ import json
 f = open( '../data/at_large_county_data.csv', 'rU' )
 reader = csv.DictReader( f, dialect='excel')
 
+fixed = []
+
+#some counties have spaces in names, we need ID field with no spaces to match up with SVG IDs
+for row in reader:
+    cID = row['COUNTY'].capitalize()
+    if ' ' in cID:
+        cID = cID.split()[0]
+
+    row['cID'] = cID
+    fixed.append(row)
+
 # Parse the CSV into JSON
-out = json.dumps( [ row for row in reader ] )
+out = json.dumps( fixed, indent=4)
 print "JSON parsed!"
 # Save the JSON
 f = open( '../data/districts.json', 'w')
