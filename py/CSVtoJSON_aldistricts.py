8e1c33059972618e7752ee987d8f0b290e0e7ef0
Emily Merwin <emilymerwinajc@gmail.com>
2013-11-11 19:57:06 -0500

    Use better field names in JSON

--- py/CSVtoJSON_aldistricts.py
+++ py/CSVtoJSON_aldistricts.py
@@ -3,13 +3,13 @@ import csv
 import json
 
 f = open( '../data/at_large_county_data.csv', 'rU' )
-reader = csv.DictReader( f, dialect='excel')
-
+reader = csv.DictReader( f, fieldnames = ( "county", "blkvoters", "blkofficials", "representation", "category" ), dialect='excel') #doing it this way will put row 1 into your data - exclude fieldnames to use row 1 as fieldnames
+reader.next()  # skip original header row
 fixed = []
 
 #some counties have spaces in names, we need ID field with no spaces to match up with SVG IDs
 for row in reader:
-    cID = row['COUNTY'].capitalize()
+    cID = row['county'].capitalize()
     if ' ' in cID:
         cID = cID.split()[0]
 
