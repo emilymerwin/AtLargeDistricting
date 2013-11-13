c7f93e86b89217a7c56c91e04b5555cc19b4bdac
Emily Merwin <emilymerwinajc@gmail.com>
2013-11-13 14:54:41 -0500

    Convert decimal percents to whole numbers in parser

--- py/CSVtoJSON_aldistricts.py
+++ py/CSVtoJSON_aldistricts.py
@@ -1,12 +1,18 @@
 #! /usr/bin/env python
 import csv
 import json
+import decimal
 import re
 
 f = open( '../data/at_large_county_data.csv', 'rU' )
 reader = csv.DictReader( f, fieldnames = ( "county", "blkvoters", "blkofficials", "representation", "category" ), dialect='excel') #doing it this way will put row 1 into your data - exclude fieldnames to use row 1 as fieldnames
 reader.next()  # skip original header row
 fixed = []
+
+#convert from decimal percent format to whole number
+def rounder(n):
+    dec = int(decimal.Decimal(n)*100)
+    return str(dec)
 
 for row in reader:
     row['county'] = row['county'].title()
@@ -24,6 +30,9 @@ for row in reader:
         cID = cID.split()[0]
 
     row['cID'] = cID
+    row['blkvoters'] = rounder(row['blkvoters'])
+    row['blkofficials'] = rounder(row['blkofficials'])
+    row['representation'] = rounder(row['representation'])
     fixed.append(row)
 
 # Parse the CSV into JSON
