d0187f0cea4bfd1ea0bf182ef91c17b015a26f55
Emily Merwin <emilymerwinajc@gmail.com>
2013-11-12 21:08:18 -0500

    Fix casing on DeKalb, McIntosh and McDuffie counties

--- py/CSVtoJSON_aldistricts.py
+++ py/CSVtoJSON_aldistricts.py
@@ -1,20 +1,29 @@
 #! /usr/bin/env python
 import csv
 import json
+import re
 
 f = open( '../data/at_large_county_data.csv', 'rU' )
 reader = csv.DictReader( f, fieldnames = ( "county", "blkvoters", "blkofficials", "representation", "category" ), dialect='excel') #doing it this way will put row 1 into your data - exclude fieldnames to use row 1 as fieldnames
 reader.next()  # skip original header row
 fixed = []
 
-#some counties have spaces in names, we need ID field with no spaces to match up with SVG IDs
 for row in reader:
-    cID = row['county'].capitalize()
+    row['county'] = row['county'].title()
+
+    if re.search("Mcintosh", row['county']):
+        row['county'] = "McIntosh"
+    if re.search("Dekalb", row['county']):
+        row['county'] = "DeKalb"
+    if re.search("Mcduffie", row['county']):
+        row['county'] = "McDuffie"
+
+    #some counties have spaces in names, we need ID field with no spaces to match up with SVG IDs
+    cID = row['county']
     if ' ' in cID:
         cID = cID.split()[0]
 
     row['cID'] = cID
-    row['county'] = row['county'].title()
     fixed.append(row)
 
 # Parse the CSV into JSON
